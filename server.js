9c6d8d4f703b54932d796536be43632859156c7e
Akshay Puranik <akshay.social@gmail.com>
2022-12-13 14:27:44 +0530

    added cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const { dbConnect } = require("./src/config/db");
 const emiRoute = require("./src/routes/emiRoute");
 const userRoute = require("./src/routes/userRoute");
@@ -6,6 +7,7 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.use("/user", userRoute);
 app.use("/emi", emiRoute);
