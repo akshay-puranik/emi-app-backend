b0ffb54499925ed893d2b22a0ae5725d13d7887e
Akshay Puranik <akshay.social@gmail.com>
2022-12-17 13:10:24 +0530

    Added: Cors

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const dbConnect = require("./configs/db");
 const jobRoute = require("./routes/job.routes");
+const cors = require(cors);
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/job",jobRoute);
